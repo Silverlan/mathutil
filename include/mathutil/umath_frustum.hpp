e51335ed27db40201dd9e891338d260ea885b37a
Silverlan <silverlann@gmail.com>
2019-03-13 22:42:27 +0100

    Added frustum functions Fixed uvec::calc_world_direction_from_2d_coordinates returning incorrect values

---
+++ include/mathutil/umath_frustum.hpp
@@ -0,0 +1,24 @@
+#ifndef __UMATH_FRUSTUM_HPP__
+#define __UMATH_FRUSTUM_HPP__
+
+#include "mathutildefinitions.h"
+#include "umath.h"
+#include "uvec.h"
+#include <array>
+
+namespace umath
+{
+	namespace frustum
+	{
+		DLLMUTIL Vector3 get_far_plane_center(const Vector3 &pos,const Vector3 &forward,float farZ);
+		DLLMUTIL Vector3 get_near_plane_center(const Vector3 &pos,const Vector3 &forward,float nearZ);
+		DLLMUTIL void get_near_plane_size(float fovRad,float nearZ,float aspectRatio,float &outNearW,float &outNearH);
+		DLLMUTIL void get_far_plane_size(float fovRad,float farZ,float aspectRatio,float &outFarW,float &outFarH);
+		DLLMUTIL std::array<Vector3,4> get_far_plane_boundaries(const Vector3 &pos,const Vector3 &forward,const Vector3 &up,float fovRad,float farZ,float aspectRatio,float *outFarW,float *outFarH);
+		DLLMUTIL std::array<Vector3,4> get_near_plane_boundaries(const Vector3 &pos,const Vector3 &forward,const Vector3 &up,float fovRad,float nearZ,float aspectRatio,float *outNearW,float *outNearH);
+		DLLMUTIL Vector3 get_near_plane_point(const Vector3 &pos,const Vector3 &forward,const Vector3 &right,const Vector3 &up,float fovRad,float nearZ,float aspectRatio,const Vector2 &uv);
+		DLLMUTIL Vector3 get_far_plane_point(const Vector3 &pos,const Vector3 &forward,const Vector3 &right,const Vector3 &up,float fovRad,float farZ,float aspectRatio,const Vector2 &uv);
+	};
+};
+
+#endif
