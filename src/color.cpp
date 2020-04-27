85b0a515cec2ccec0a4431fd4c6c439501660d24
Silverlan <silverlann@gmail.com>
2020-04-27 12:15:25 +0200

    Fixed initializing Color with a Vector3 setting the alpha to 0 instead of 255

--- src/color.cpp
+++ src/color.cpp
@@ -32,7 +32,7 @@ Color::Color(Int16 r,Int16 g,Int16 b,Int16 a)
 }
 Color::Color(const std::string &str) {ustring::string_to_array<Int16,Int32>(str,&r,atoi,4);}
 Color::Color(const Vector3 &v)
-	: r(static_cast<Int16>(v.x *255.f)),g(static_cast<Int16>(v.y *255.f)),b(static_cast<Int16>(v.z *255.f))
+	: r(static_cast<Int16>(v.x *255.f)),g(static_cast<Int16>(v.y *255.f)),b(static_cast<Int16>(v.z *255.f)),a(255)
 {}
 Color::Color(const Vector4 &v)
 	: r(static_cast<Int16>(v.x *255.f)),g(static_cast<Int16>(v.y *255.f)),b(static_cast<Int16>(v.z *255.f)),a(static_cast<Int16>(v.w *255.f))
