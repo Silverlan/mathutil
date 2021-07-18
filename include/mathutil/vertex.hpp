c01ed4461efe8ba1dc12b87b511a1a0d9919d2e9
Silverlan <silverlann@gmail.com>
2021-07-18 07:40:12 +0200

    Add 'Vertex' and 'VertexWeight' structs

---
+++ include/mathutil/vertex.hpp
@@ -0,0 +1,48 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef __MATHUTIL_VERTEX_HPP__
+#define __MATHUTIL_VERTEX_HPP__
+
+#include "mathutil/mathutildefinitions.h"
+#include "mathutil/uvec.h"
+
+#pragma warning(push)
+#pragma warning(disable : 4251)
+namespace umath
+{
+	static const auto VERTEX_EPSILON = 0.001f;
+
+	struct DLLMUTIL Vertex
+	{
+		Vertex(const Vector3 &position,const Vector2 &uv,const Vector3 &normal,const Vector4 &tangent);
+		Vertex(const Vector3 &position,const Vector2 &uv,const Vector3 &normal);
+		Vertex(const Vector3 &position,const Vector3 &normal);
+		Vertex();
+		bool Equal(const Vertex &other,float epsilon) const;
+		bool operator==(const Vertex &other) const;
+		bool operator!=(const Vertex &other) const;
+		Vector3 position = {};
+		Vector2 uv = {};
+		Vector3 normal = {};
+		Vector4 tangent = {}; // w-component is handedness
+		Vector3 GetBiTangent() const;
+	};
+
+	struct DLLMUTIL VertexWeight
+	{
+		VertexWeight(const Vector4i &boneIds,const Vector4 &weights);
+		VertexWeight();
+		bool operator==(const VertexWeight &other) const;
+		bool operator!=(const VertexWeight &other) const;
+		Vector4i boneIds = {};
+		Vector4 weights = {};
+	};
+};
+#pragma warning(pop)
+
+DLLMUTIL std::ostream &operator<<(std::ostream &out,const umath::Vertex &v);
+DLLMUTIL std::ostream &operator<<(std::ostream &out,const umath::VertexWeight &v);
+
+#endif
---
+++ src/vertex.cpp
@@ -0,0 +1,76 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "mathutil/vertex.hpp"
+
+std::ostream &operator<<(std::ostream &out,const umath::Vertex &v)
+{
+	out<<"Vertex["<<v.position<<"]["<<v.normal<<"]["<<v.uv<<"]";
+	return out;
+}
+
+std::ostream &operator<<(std::ostream &out,const umath::VertexWeight &v)
+{
+	out<<"VertexWeight["<<v.boneIds<<"]["<<v.weights<<"]";
+	return out;
+}
+
+///////////////////////////////
+
+umath::Vertex::Vertex(const Vector3 &_position,const Vector2 &_uv,const Vector3 &_normal,const Vector4 &_tangent)
+	: position(_position),uv(_uv),normal(_normal),tangent(_tangent)
+{}
+
+umath::Vertex::Vertex(const Vector3 &position,const Vector2 &uv,const Vector3 &normal)
+	: Vertex(position,uv,normal,Vector4{0.f,0.f,0.f,1.f})
+{}
+
+umath::Vertex::Vertex(const Vector3 &position,const Vector3 &normal)
+	: Vertex(position,Vector2(0.f,0.f),normal)
+{}
+
+umath::Vertex::Vertex()
+	: Vertex(Vector3{0.f,0.f,0.f},Vector3{0.f,0.f,0.f})
+{}
+
+Vector3 umath::Vertex::GetBiTangent() const
+{
+	return tangent.w *uvec::cross(normal,Vector3{tangent});
+}
+
+bool umath::Vertex::Equal(const Vertex &other,float epsilon) const
+{
+	return (
+		umath::abs(position.x -other.position.x) <= VERTEX_EPSILON && umath::abs(position.y -other.position.y) <= VERTEX_EPSILON && umath::abs(position.z -other.position.z) <= VERTEX_EPSILON &&
+		umath::abs(uv.x -other.uv.x) <= VERTEX_EPSILON && umath::abs(uv.y -other.uv.y) <= VERTEX_EPSILON &&
+		umath::abs(normal.x -other.normal.x) <= VERTEX_EPSILON && umath::abs(normal.y -other.normal.y) <= VERTEX_EPSILON && umath::abs(normal.z -other.normal.z) <= VERTEX_EPSILON
+	) ? true : false;
+}
+
+bool umath::Vertex::operator==(const Vertex &other) const {return Equal(other,VERTEX_EPSILON);}
+bool umath::Vertex::operator!=(const Vertex &other) const
+{
+	return (*this == other) ? false : true;
+}
+
+///////////////////////////////
+
+umath::VertexWeight::VertexWeight(const Vector4i &_boneIds,const Vector4 &_weights)
+	: boneIds(_boneIds),weights(_weights)
+{}
+
+umath::VertexWeight::VertexWeight()
+	: VertexWeight(Vector4i{-1,-1,-1,-1},Vector4{0.f,0.f,0.f,0.f})
+{}
+
+bool umath::VertexWeight::operator==(const VertexWeight &other) const
+{
+	return (boneIds == other.boneIds &&
+		umath::abs(weights.x -other.weights.x) <= VERTEX_EPSILON && umath::abs(weights.y -other.weights.y) <= VERTEX_EPSILON && umath::abs(weights.z -other.weights.z) <= VERTEX_EPSILON && umath::abs(weights.w -other.weights.w) <= VERTEX_EPSILON
+	) ? true : false;
+}
+bool umath::VertexWeight::operator!=(const VertexWeight &other) const
+{
+	return (*this == other) ? false : true;
+}
